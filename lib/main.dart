8be888fde6fa40a84914e18838ee0fe650c5bc2a
Lino Shaji <linoshaji23@gmail.com>
2022-10-04 11:18:09 +0530

    cleaned home UI parent issue and asset loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:adhakr/assets/test.dart';
+import 'package:adhakr/test.dart';
 import 'package:adhakr/home.dart';
 import 'package:flutter/material.dart';
 
---
+++ lib/test.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class test extends StatefulWidget {
+  const test({Key? key}) : super(key: key);
+
+  @override
+  State<test> createState() => _testState();
+}
+
+class _testState extends State<test> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Column(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Container(
+                    height: MediaQuery.of(context).size.height / 3.5,
+                    color: Colors.white,
+                    child: Column(children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("hi"),
+                          Text(
+                            "As-salamu alaykum",
+                            style: TextStyle(
+                                fontSize: 25, color: Color(0xff106cb0)),
+                          )
+                        ],
+                      )
+                    ])),
+              ),
+              Flexible(
+                flex: 6,
+                child: Container(
+                    height: MediaQuery.of(context).size.height / 1.3,
+                    decoration: const BoxDecoration(
+                        color: Color(0xff4d90cd),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20))),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            color: Color(0xff366895),
+                            margin: const EdgeInsets.only(
+                              top: 50,
+                            ),
+                            child: Column(children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: const [
+                                  Padding(
+                                    padding: EdgeInsets.all(9.0),
+                                    child: Text("DHUHR",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 20)),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(9.0),
+                                    child: Text("30  SAFAR",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 20)),
+                                  )
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: const [
+                                  Padding(
+                                    padding: EdgeInsets.all(9.0),
+                                    child: Text("12:34 pm",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 20)),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.all(9.0),
+                                    child: Text("1444",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 20)),
+                                  )
+                                ],
+                              ),
+                            ]),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Container(
+                              height: 120,
+                              width: 300,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: const Align(
+                                alignment: Alignment.center,
+                                child: Text("MORNING ADHKAR",
+                                    style: TextStyle(
+                                        color: Color(0xff106cb0),
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Container(
+                              height: 120,
+                              width: 300,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: const Align(
+                                alignment: Alignment.center,
+                                child: Text("EVENING ADHKAR",
+                                    style: TextStyle(
+                                        color: Color(0xff106cb0),
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                            ),
+                          ),
+                          Image.asset("lib/assets/homeui.png", height: 70),
+                          const Text(
+                            "\"Keep your tounge wet with\nthe rememberence of Allah\"",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ])),
+              )
+            ],
+          ),
+        ));
+  }
+}
